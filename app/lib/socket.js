23cbacb76fa6f95a359d5a3015c62dc80cbac160
Andre <amnarciso@gmail.com>
2026-04-22 09:30:37 -0300

    temp: dev local ip

--- app/lib/socket.js
+++ app/lib/socket.js
@@ -7,7 +7,7 @@ import { getFbInfo, renderImageToFramebuffer, renderBufferToFramebuffer } from "
 const DEVICE_ID = getSerialNumber();
 
 export function initSocket() {
-  const socket = io("http://192.168.2.144:3000", {
+  const socket = io("http://192.168.1.102:3000", {
     transports: ["websocket"], // força websocket (melhor pro embedded)
     reconnection: true,
     reconnectionAttempts: Infinity,
